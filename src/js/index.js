966f6c88647b4c694ac536733753d78a865e52b7
maciejziemichod <maciej.ziemichod@gmail.com>
2021-02-28 13:24:29 +0100

    feat: mobile version done

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,22 @@ const menuButtonShow = document.querySelector(".header__menu-button--show");
 const menuButtonHide = document.querySelector(".header__menu-button--hide");
 const menuContent = document.querySelector(".header__menu-content");
 
+const searchButtonShow = document.querySelector(".header__search-button--show");
+const searchButtonHide = document.querySelector(".header__search-button--hide");
+const searchFormContainer = document.querySelector(
+    ".header__search-form-container"
+);
+
 menuButtonShow.addEventListener("click", () => {
     menuContent.classList.add("visible");
 });
 menuButtonHide.addEventListener("click", () => {
     menuContent.classList.remove("visible");
+});
+
+searchButtonShow.addEventListener("click", () => {
+    searchFormContainer.classList.add("visible");
+});
+searchButtonHide.addEventListener("click", () => {
+    searchFormContainer.classList.remove("visible");
 });
