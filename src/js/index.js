a243af0dfbf543823aebe09bc6ccbb4c75d64873
maciejziemichod <maciej.ziemichod@gmail.com>
2021-02-28 17:29:36 +0100

    feat: large screens grid layout

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,6 @@
 const menuButtonShow = document.querySelector(".header__menu-button--show");
 const menuButtonHide = document.querySelector(".header__menu-button--hide");
 const menuContent = document.querySelector(".header__menu-content");
-
-const searchButtonShow = document.querySelector(".header__search-button--show");
-const searchButtonHide = document.querySelector(".header__search-button--hide");
-const searchFormContainer = document.querySelector(
-    ".header__search-form-container"
-);
-
 menuButtonShow.addEventListener("click", () => {
     menuContent.classList.add("visible");
 });
@@ -17,6 +10,11 @@ menuButtonHide.addEventListener("click", () => {
     menuContent.classList.remove("visible");
 });
 
+const searchButtonShow = document.querySelector(".header__search-button--show");
+const searchButtonHide = document.querySelector(".header__search-button--hide");
+const searchFormContainer = document.querySelector(
+    ".header__search-form-container"
+);
 searchButtonShow.addEventListener("click", () => {
     searchFormContainer.classList.add("visible");
 });
